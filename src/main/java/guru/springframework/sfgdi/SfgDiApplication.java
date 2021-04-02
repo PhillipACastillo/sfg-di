8536f6c09b89e35d0f04efa57b0476617c845288
Phillip Castillo <PhillipACastillo@gmail.com>
2021-04-02 16:05:18 -0700

    Revert "Configure proper Spring profiles"
    This reverts commit 8b14356ab7627267520631975dfe066d29e07d9f.

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgdi;
 
-import guru.springframework.sfgdi.controllers.*;
+import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
+import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,13 +16,9 @@ public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
-		System.out.println("-------- International");
-		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
-		System.out.println(i18nController.sayHello());
-
-		System.out.println("----------- Primary");
 		MyController myController = (MyController) ctx.getBean("myController");
-		System.out.println(myController.getGreeting());
+		String greeting = myController.sayHello();
+		System.out.println(greeting);
 
 		System.out.println("---------- Property");
 		PropertyInjectedController propertyInjectedController =
